cf598a9f28f9e45d2ae13b6f3dc26e39c48d969c
Alex Pullen <alex.pullen@is.co.za>
2018-08-22 14:55:07 +0200

    - Fixed schema creation syntax.

--- dbi/schemas/transactionSchema.js
+++ dbi/schemas/transactionSchema.js
@@ -4,13 +4,13 @@ module.exports = {
     destinationDomain: String,
     uuid: { type: String, index: true },
     status: String,
-    pid: { type: String, index: true},
+    pid: { type: String, index: true },
     lambda: String,
     request: { type: Object, default: {} },
     responses: { type: Array, default: [] },
     latency: { type: Object, default: {} },
     version: String,
     lastDeviceResponse: { type: Object, default: {} }
-    createdAt: { type: Date, expires: 90, default: Date.now }
+    createdAt: { type: Date, default: Date.now, index: { expires: 90 } }
 
 };
