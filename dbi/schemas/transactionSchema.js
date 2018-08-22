784a858e017ddc1b81cdf626f2167ee42146fee8
Alex Pullen <alex.pullen@is.co.za>
2018-08-22 12:58:53 +0200

    - Added indexes to domain and pid fields. - Added 90 second TTL (for testing purposes) to transactionsSchemas. This will be increased for prod.

--- dbi/schemas/transactionSchema.js
+++ dbi/schemas/transactionSchema.js
@@ -1,15 +1,16 @@
 module.exports = {
 
-    domain: String,
+    domain: { type: String, index: true },
     destinationDomain: String,
     uuid: { type: String, index: true },
     status: String,
-    pid: String,
+    pid: { type: String, index: true},
     lambda: String,
     request: { type: Object, default: {} },
     responses: { type: Array, default: [] },
     latency: { type: Object, default: {} },
     version: String,
     lastDeviceResponse: { type: Object, default: {} }
+    createdAt: { type: Date, expires: 90, default: Date.now }
 
 };
